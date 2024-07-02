fa5b0429998245272d4d35368ab95d1b6410f158
GaboJohn <johngabotsalelwe@gmail.com>
2024-07-02 13:54:46 +0200

    Shapes, Loops, and Keys

--- 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CourseListRow from './CourseListRow';
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
 
-describe('CourseListRow', () => {
-  it('renders one cell with colspan = 2 when textSecondCell does not exist and isHeader is true', () => {
-    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="Test" />);
-    expect(wrapper.find('th').prop('colSpan')).toEqual('2');
+describe("Course List Row component test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+    expect(wrapper.exists()).toBe(true);
   });
 
-  it('renders two cells when textSecondCell is present and isHeader is true', () => {
-    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="Test" textSecondCell="Test2" />);
-    expect(wrapper.find('th')).toHaveLength(2);
+  it("should render one cell with colspan = 2 when textSecondCell is null", () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />);
+    expect(wrapper.find("tr").children()).toHaveLength(1);
+    expect(wrapper.find("th").prop("colSpan")).toBe(2);
+    expect(wrapper.find("th").text()).toBe("test");
   });
 
-  it('renders two td elements within a tr element when isHeader is false', () => {
-    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="Test" textSecondCell="Test2" />);
-    expect(wrapper.find('td')).toHaveLength(2);
+  it("should render two cells when textSecondCell is not null", () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test" />);
+    expect(wrapper.find("tr").children()).toHaveLength(2);
+    expect(wrapper.find("th").at(0).text()).toBe("test");
+    expect(wrapper.find("th").at(1).text()).toBe("test");
+  });
+
+  it("should render two td cells when isHeader is false", () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test" />);
+    expect(wrapper.find("tr").children()).toHaveLength(2);
+    expect(wrapper.find("td").at(0).text()).toBe("test");
+    expect(wrapper.find("td").at(1).text()).toBe("test");
   });
 });
--- 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,63 +1,56 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { getLatestNotification } from "../utils/utils";
-import Notifications from "./Notifications";
-import NotificationItem from "./NotificationItem";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
+import { getLatestNotification } from '../utils/utils';
 
-describe("Notification tests", () => {
-  it("renders Notification component without crashing", () => {
+describe('Notification tests', () => {
+  it('renders Notification component without crashing', () => {
     const wrapper = shallow(<Notifications />);
-
     expect(wrapper).toBeDefined();
   });
 
-  it("renders correct list items", () => {
-    const wrapper = shallow(<Notifications displayDrawer={true} />);
-    expect(wrapper.find("ul").children()).toHaveLength(3);
-    wrapper.find("ul").forEach((node) => {
-      expect(node.equals(<NotificationItem />));
-    });
-    expect(wrapper.find("ul").childAt(0).html()).toEqual('<li data-notification-type="default">New course available</li>');
-    expect(wrapper.find("ul").childAt(1).html()).toEqual('<li data-notification-type="urgent">New resume available</li>');
-    expect(wrapper.find("ul").childAt(2).html()).toEqual(`<li data-urgent=\"true\">${getLatestNotification()}</li>`);
+  it('renders correct list items', () => {
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+    ];
+    const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+    expect(wrapper.find('ul').children()).toHaveLength(3);
+    expect(wrapper.find(NotificationItem).first().prop('value')).toEqual('New course available');
+    expect(wrapper.find(NotificationItem).at(1).prop('value')).toEqual('New resume available');
+    expect(wrapper.find(NotificationItem).last().prop('html')).toEqual({ __html: getLatestNotification() });
   });
 
-  it("renders an unordered list", () => {
-    const wrapper = shallow(<Notifications displayDrawer={true} />);
-    expect(wrapper.find("ul").children()).toHaveLength(3);
-    wrapper.find("ul").forEach((node) => {
-      expect(node.equals(<NotificationItem />));
-    });
+  it('renders an unordered list', () => {
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+    ];
+    const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+    expect(wrapper.find('ul').children()).toHaveLength(3);
   });
 
-  it("renders correct text", () => {
+  it('renders correct text', () => {
     const wrapper = shallow(<Notifications displayDrawer={true} />);
-
     expect(wrapper.contains(<p>Here is the list of notifications</p>)).toBe(true);
   });
 
-  it("displays menu item when displayDrawer is false", () => {
+  it('displays menu item when displayDrawer is false', () => {
     const wrapper = shallow(<Notifications displayDrawer={false} />);
-
-    expect(wrapper.find("div.menuItem").exists()).toBe(true);
-    expect(wrapper.find("div.menuItem").html()).toEqual('<div class="menuItem"><p>Your notifications</p></div>');
+    expect(wrapper.find('div.menuItem').exists()).toBe(true);
+    expect(wrapper.find('div.menuItem').html()).toEqual('<div class="menuItem"><p>Your notifications</p></div>');
   });
 
-  it("does not display notifications when displayDrawer is false", () => {
+  it('does not display notifications when displayDrawer is false', () => {
     const wrapper = shallow(<Notifications displayDrawer={false} />);
-
-    expect(wrapper.find("div.Notifications").exists()).toBe(false);
+    expect(wrapper.find('div.Notifications').exists()).toBe(false);
   });
 
-  it("does not display menuItem when displayDrawer is true", () => {
+  it('displays Notifications when displayDrawer is true', () => {
     const wrapper = shallow(<Notifications displayDrawer={true} />);
-
-    expect(wrapper.find("div.menuItem").exists()).toBe(true);
-  });
-
-  it("displays Notifications when displayDrawer is true", () => {
-    const wrapper = shallow(<Notifications displayDrawer={true} />);
-
-    expect(wrapper.find("div.Notifications").exists()).toBe(true);
+    expect(wrapper.find('div.Notifications').exists()).toBe(true);
   });
 });
